8d2a44ce239c7fde4bb1b83124ea79bf41fb1dee
cparker2310 <55166461+cparker2310@users.noreply.github.com>
2023-02-08 12:10:35 -0500

    front end set up

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const mongoose= require('mongoose');
-const request= require('request');
-const cookieParser= require('cookie-parser');
 const express = require('express');
-const routes= require('./routes/routes');
-const { requireAuthen, checkUser } = require('./middleware/middleware');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
 const app = express();
 
+// middleware
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
+// view engine
 app.set('view engine', 'ejs');
 
-const dbURI = 'mongodb+srv://Barbara:JhqipbEQE1Wc6qDS@cluster0.cxdjlov.mongodb.net/?retryWrites=true&w=majority';
+// database connection
+const dbURI = 'mongodb+srv://cparker2310:astonsoccer@cluster0.2sqxf.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
+// routes
 app.get('*', checkUser);
-app.get('/', (request, response) => response.render('home'));
-app.get('/options', requireAuthen, (request, response) => response.render('options'));
-app.use(routes);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
--- models/User.js
+++ models/User.js
@@ -1,46 +1,43 @@
-const mongoose= require('mongoose');
-const isEmail= require('validator');
-const bcrypt= require('bcrypt');
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
-const userSchema= new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, 'Please enter a valid email']
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minlength: [8, 'Password length must be a minimum of 8 characters']
-    },
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
 });
 
-userSchema.post('save', function (document, next) {
-    console.log('New Profile Saved Successfully', document);
-    next();
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-userSchema.pre('save', async function (next) {
-    const salt= await bcrypt.genSalt();
-    this.password= await bcrypt.hash(this.password, salt);
-    next();
-});
-
-userSchema.statics.login= async function (email, password) {
-    const user= await this.findOne({email});
-    if (user)
-    {
-        const authenUser= await bcrypt.compare(password, user.password);
-        if (authenUser)
-        {
-            return user;
-        }
-        throw Error('Password is Incorrect');
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
     }
-    throw Error('Email is not registered');
-}
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 
-const User= mongoose.model('user', userSchema)
-module.exports= User;
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
