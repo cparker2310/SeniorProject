18d1332cbd99840590be579f6e4358f9be2c81fa
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-03-01 22:43:47 -0500

    adding navbar and background to other pages

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/NavBar/Navbar';
+import Castle from '../images/castle.jpg';
+import styled from 'styled-components';
+
+const Background= styled.section`
+    background-image: url(${Castle});
+    height: 100%;
+    width: 100%;
+    display: block;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: fixed;
+`;
 
 const MessageBoard = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <Background>
+        
+      </Background>
+    </>
   )
 }
 
