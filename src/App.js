cc79a3105380958a8e1840a7ae10368959d72580
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-03-09 22:04:44 -0500

    forgot to uncomment edit profile

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Directory from './pages/NewDirectory/directory';
 import CareerCenter from './pages/jobs';
 import MessageBoard from './pages/messages';
 import Form from './components/MultiStepForm/Form';
-//import EditProfile from './pages/Profile/EditProfile';    <Route path="/editprofile" element={<EditProfile />} exact /> 
+import EditProfile from './pages/Profile/EditProfile';
 
 function App() {
     return (
@@ -24,6 +24,7 @@ function App() {
                     <Route path="/directory" element={<Directory />} exact />
                     <Route path="/jobs" element={<CareerCenter />} exact />
                     <Route path="/messages" element={<MessageBoard />} exact />
+                    <Route path="/editprofile" element={<EditProfile />} exact /> 
                 </Routes>
             </Router>
 
