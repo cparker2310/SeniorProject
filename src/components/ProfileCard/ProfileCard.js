ac1c7e906dc56ea45aef1d4890cc875864c514fc
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-03-15 21:59:16 -0400

    #11 commented out link to old edit profile

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -70,7 +70,7 @@ export default (props) => {
                 <Typography variant='h8' color='#030000'>Email: {user.email2} </Typography>
             </CardContent>
             <CardActions>
-                <Button variant='contained' as="a" href='/editprofile' onClick={props.openEdit}>Edit Profile</Button>
+                <Button variant='contained' /*as="a" href='/editprofile'*/ onClick={props.openEdit}>Edit Profile</Button>
                 <Button variant='contained' >Delete Profile</Button>
             </CardActions>
         </Card>
