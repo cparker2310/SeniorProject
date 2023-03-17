11724de0516f91fe61457999933f806c81a79930
Barbara-Kavanaugh <55026849+Barbara-Kavanaugh@users.noreply.github.com>
2023-03-16 20:25:42 -0400

    #1 working on profile picture

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const multer = require('multer')
+const User = require('./models/user-model')
 
 const db = require('./db')
 const router = require('./routes/router')
@@ -18,7 +20,34 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+const Storage = multer.diskStorage({
+    destination: 'uploaded images',
+    filename: (req, res, cb) => {
+        cb(null, file.orginalname)
+    }
+})
+
+const uploadImage = multer({
+    storage: Storage
+}).single('testImage')
+
+
 app.use('/api', router)
+
+app.post('/upload', (req, res) => {
+    uploadImage(req, res, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            const newImage = new User({
+                name: req.body.name,
+                image: { data: req.file.filename, contentType: 'image/png'}
+            })
+            newImage.save().then(() => res.send('Image Successfully Uploaded')).catch((err) => console.log(err))
+        }
+    })
+})
 
 module.exports = app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
