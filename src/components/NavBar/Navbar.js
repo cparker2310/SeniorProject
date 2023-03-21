caa401ebb477daf1680a26a03b5d01ecfb174ca0
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-03-20 22:56:04 -0400

    fixing file path

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
-import { useSelector } from 'react-redux';
 
 export const NavbarContainer= styled.div`
   width: 100%;
