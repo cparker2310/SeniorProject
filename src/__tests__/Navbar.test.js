439a7612e90185eb684b305b7085b6ed15a806de
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-03-24 11:15:38 -0400

    frontend testing

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,6 +1,13 @@
 import renderer from 'react-test-renderer';
-
 import Navbar from '../components/NavBar/Navbar';
+import Castle from '../images/castle.jpg';
+
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+expect.extend({ toMatchImageSnapshot });
+
+it('Background image renders correctly', () => {
+    expect(Castle).toMatchImageSnapshot();
+});
 
 it('Navbar renders correctly', () => {
     const tree= renderer.create(<Navbar />).toJSON();
