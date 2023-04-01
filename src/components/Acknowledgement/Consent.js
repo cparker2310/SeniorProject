2a45dff6c34f32fac8f948c3be330d07abcc8252
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-04-01 13:15:17 -0400

    made acknowledgement look better

--- src/components/Acknowledgement/Consent.js
+++ src/components/Acknowledgement/Consent.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import './Consent.css';
+import { MdClose } from "react-icons/md";
 import Castle from '../../images/castle.jpg';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Background= styled.section`
     background-image: url(${Castle});
@@ -13,106 +15,53 @@ const Background= styled.section`
     position: fixed;
 `;
 
-const Proceed= styled(Link)`
-    padding: 3px 18px;
-    justify-content: center;
-    border-radius: 4px;
-    font-family: 'Lora', serif;
-    border: none;
-    background: #a32738;
-    color: #fdfdfd;
-    font-size: 14px;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    text-decoration: none;
-    position: relative;
-    margin-left: auto;
-    margin-right: auto;
-
-    &:hover {
-        background: #60000D;
-        transition: all 0.2s ease-in-out;
-    }
+export const Icon= styled(Link)`
+  margin-left: 28px;
+  margin-top: 28px;
+  text-decoration: none;
+  color: #d12626;
+  font-weight: 800;
+  font-size: 34px;
+  text-shadow: 1.5px 1.5px 2.5px #030000;
 `;
 
-const Cancel= styled(Link)`
-    padding: 3px 18px;
-    justify-content: center;
-    border-radius: 4px;
-    font-family: 'Lora', serif;
-    border: none;
-    background: #63625d;
-    color: #fdfdfd;
-    font-size: 14px;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    text-decoration: none;
-    position: relative;
-    margin-left: auto;
-    margin-right: auto;
-
-    &:hover {
-        background: #2e2d2c;
-        transition: all 0.2s ease-in-out;
-    }
-`;
-
-const Container= styled.div`
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    font-family: 'Lora', serif;
-    align-items: center;
-`;
-
-const BoxWrapper= styled.div`
-    border-radius: 10px;
-    width: 800px;
-    height: 500px;
-    box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-    background: #fdfdfd;
-    color: #030000;
-    display: flex;
-    top: 100px;
-    right: 300px;
-    left: 300px;
-    position: relative;
-    z-index: 10;
-`;
-
-const Content= styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    line-height: 2.8;
-    color: #030000;
-    margin-bottom: 1rem;
-    top: 5px;
-    right: 100px;
-    left: 70px;
-    font-size: 18px;
-    position: relative;
-    text-align: center;
-`;
 
 const Consent = () => {
   return (
+    <>
     <Background>
-        <Container>
-            <BoxWrapper>
-                <Content>
-                    Acknowledgment <br />
-                    By wishing to proceed, 
-                    I acknowledge that 
-                    I have read, understand, 
-                    and agree to sharing my personal 
-                    information on the Maryvale Alumnae Network.
-                    <Cancel to="/">Cancel</Cancel>
-                    <Proceed to="/signup">Proceed</Proceed>
-                </Content>
-            </BoxWrapper>
-        </Container>
+    <Icon to='/'>MARYVALE</Icon>
+    <div className="modalContainer">
+        <div className="titleCloseBtn">
+            <a href="/">
+                <button> <MdClose /> </button>
+            </a>
+        </div>
+
+        <div className="title">
+            <h1>Acknowledgment</h1>
+        </div>
+
+        <div className="body">
+            <p> By wishing to proceed, 
+                I acknowledge that 
+                I have read, understand, 
+                and agree to sharing my personal 
+                information on the Maryvale Alumnae Network.
+            </p>
+        </div>
+
+        <div className="footer">
+            <a href="/">
+                <button id="cancelBtn">Cancel</button>
+            </a>
+            <a href="/signup">
+                <button>Proceed</button>
+            </a>
+        </div>
+    </div>
     </Background>
+    </>
   );
 }
 
