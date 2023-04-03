6d0a881535ae64c35050f6afdd552c222d1de951
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-04-02 23:20:52 -0400

    #4 search is starting to work, needs better styling

--- src/components/Directory/Header/index.js
+++ src/components/Directory/Header/index.js
@@ -2,28 +2,34 @@ import React, { useState, useEffect } from 'react';
 import SearchBar from "material-ui-search-bar";
 import Box from "@mui/material/Box";
 import { Grid } from '@material-ui/core';
-//import LinearProgress from '@mui/material/LinearProgress';
+import api from '../../../api/index';
 
 export default (props, onChange) => {
     const [searched, setSearched]= useState("");
-    //const [names, setNames]= useState(users);
+    const [users, setUsers]= useState([]);
+    const [searchedUsers, setSearchedUsers]= useState([]);
 
-    const cancelSearch= () => {
+    useEffect(() => {
+        const searchUsers= async () => {
+            const result = await api.getAllUsers();
+            setUsers(result.data.data);
+            setSearchedUsers(result.data.data);
+        };
+        searchUsers();
+    }, []);
+
+    const cancelSearch = () => {
         setSearched("");
+        setSearchedUsers(users);
     };
 
-    /*const filterNames= event => {
-        const search= event.target.value.toLowerCase();
-        const filterNames= users.filter(names => names.toLowerCase().includes(search));
-        setNames(filterNames);
-    }
-
-     <ul>
-        {names.map(user => {
-            return <li key={user._id}>{user.firstName}</li>
-        })}
-    </ul>
-    */
+    const searchNames= (event) => {
+        const search= event.toLowerCase();
+        const searchedNames= users.filter((user) =>
+            user.firstName.toLowerCase().includes(search)
+        );
+        setSearchedUsers(searchedNames);
+    };
 
     return (
         <Box py={10}>
@@ -34,11 +40,16 @@ export default (props, onChange) => {
                             fullWidth
                             value={searched} 
                             onCancelSearch={() => cancelSearch()}
-                            //onChange={(event) => filterNames(event)}
+                            onChange={(event) => searchNames(event)}
                         />
                     </Box>
                 </Grid>
             </Grid>
+            <ul>
+                {searchedUsers.map((user) => {
+                    return <li key={user._id}>{user.firstName}</li>;
+                })}
+            </ul>
         </Box>
     );
-}
+};
