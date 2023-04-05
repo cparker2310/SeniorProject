d17da673bc00eaabc389639b456585cc2ba34f80
cparker2310 <55166461+cparker2310@users.noreply.github.com>
2023-04-05 18:20:34 -0400

    Update profile.js

--- src/pages/Profile/profile.js
+++ src/pages/Profile/profile.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import NavBar from '../../components/NavBar/Navbar';
 import EditProfile from '../../components/ProfileCard/EditProfile/EditProfile';
 import ProfileCard from '../../components/ProfileCard/ProfileCard';
-import api from '../../api/index'
 import { useLocation } from 'react-router-dom'
 
 
@@ -10,14 +9,14 @@ const ProfilePage = () => {
   const [edit, setEdit]= useState(false);
   const location = useLocation()
   const { props } = location.state
-  
-
+  //console.log(props)
+  const [newProps, setNew] = useState()
   
   return (
     <>
         <NavBar />
-        <EditProfile closeEdit={() => setEdit(false)} edit={edit}/>
-        <ProfileCard props={props} openEdit={() => setEdit(true)}/>
+        <EditProfile props={props} closeEdit={() => setEdit(false)} edit={edit} newProps={() => setNew(newProps)}/>
+        <ProfileCard props={newProps || props} openEdit={() => setEdit(true)}/>
     </>
   );
 }
