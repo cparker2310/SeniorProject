dd726b7b3f26b7b0a84b1fb2619e6c53dd308f92
Barbara_Kavanaugh <bjkavanaugh@loyola.edu>
2023-04-07 19:15:41 -0400

    #5 filter class year is starting to work

--- src/components/Directory/Filter/index.js
+++ src/components/Directory/Filter/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from "@mui/material/Box";
 import { 
     Button,
@@ -6,6 +6,7 @@ import {
     MenuItem,
     makeStyles
 } from '@material-ui/core';
+import api from '../../../api/index';
 
 const useStyles= makeStyles({
     wrapper: {
@@ -26,9 +27,31 @@ const useStyles= makeStyles({
 export default props => {
     const classes= useStyles();
     
+    const [users, setUsers]= useState([]);
+    const [classYears, setClassYears] = useState([]);
+
     const [userSearch, setUserSearch]= useState({
         years: '2022'
     });
+
+    useEffect(() => {
+        const getClassYears= async () => {
+            const response= await api.getAllUsers();
+            const years= response.data.map(user => user.classYear);
+            const allYears= Array.from(new Set(years));
+            setClassYears(allYears);
+        };
+        getClassYears();
+    }, []);
+
+    const filterUsers= async(filterSettings) => {
+        await api.getAllUsers().then((response) => {
+            const filteredYears= response.data.filter(
+                (users) => users.classYear=== filterSettings.classYear
+            );
+            setUsers(filteredYears);
+        })
+    }
 
 
     const handleChange= event => {
@@ -38,10 +61,8 @@ export default props => {
 
     return (
         <Box p={1} mt={-5} mb={2} className={classes.wrapper}>
-            <Select onChange={handleChange} name='years' value={userSearch.type} disableUnderline variant='filled'>
-                <MenuItem value='2022'>2022</MenuItem>
-                <MenuItem value='2021'>2021</MenuItem> 
-                <MenuItem value='2020'>2020</MenuItem>
+            <Select onChange={handleChange} name='years' value={userSearch.years} disableUnderline variant='filled'>
+                {classYears.map(year=> <MenuItem key={year} value={year}>{year}</MenuItem>)}
             </Select>
             <Button variant='contained' color='primary'>Filter</Button>
         </Box>
